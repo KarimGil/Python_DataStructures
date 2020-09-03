fb6f388e34d0e2c5105a71387733aec91e710163
KarimGil <karimg87@hotmail.com>
2020-09-03 08:02:25 -0700

    commit

--- Stack/books.py
+++ Stack/books.py
@@ -2,27 +2,27 @@ class Shelf():
     def __init__(self):
         self.books = []
 
-    def push(self, book):
+    def push (self, book):
         self.books.append(book)
 
     def pop (self):
-         return self.books.pop()
-
-    def get_stack (self):
-        return self.books
+        self.books.pop()
 
     def is_empty(self):
         return self.books == []
 
-    def peek(self):
-        if self.is_empty != []: 
-            return self.books[-1]    
+    def get_shelf(self):
+        return self.books
+
+    def peek (self):
+        if self.is_empty() == False:
+            return self.books[-1]
 
 s = Shelf()
 print(s.is_empty())
-s.push('Harry Poter')
-s.push('The Millionaire') 
-s.push('Rich Dad Poor Dad')
+s.push('Harry Potter')
+s.push('Million $')
+s.push('Rich dad poor dad')
 print(s.is_empty())
-print(s.get_stack())
-print(s.peek())  
+print(s.get_shelf())
+print(s.peek())
